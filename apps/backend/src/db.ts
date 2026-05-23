21e0d678402a8245a113843ed1902535f06cb1af
Maycon <concursosmaycon@gmail.com>
2026-05-23 10:34:37 -0300

    fix: backend

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as usersSchema from './schema/users';
